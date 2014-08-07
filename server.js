632494f812ae7b969f5527df25f3634becbfdbb0
Adetola Adewodu <adetola.adewodu@gmail.com>
2014-08-07 14:36:16 -0400

    Moved most of the files to admin directory

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var port = process.env.PORT || 3000;
 var app = express();
  
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + '/admin'));
 app.get('/', function(request, response) {
-    response.sendfile(__dirname + '/index.html');
+    response.sendfile(__dirname + '/admin/index.html');
 }).listen(port);
