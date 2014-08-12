b31595695ef2ab12c6d03bc4593a3e18c17df3f3
Adetola Adewodu <adetola.adewodu@gmail.com>
2014-08-12 17:57:54 -0400

    Added the Ordered Data Controllers and using the Orders in the Tables

--- admin/js/orders-data.js
+++ admin/js/orders-data.js
@@ -1,5 +1,28 @@
 // table data
+var orderApp = angular.module('orderApp', []);
 
-$scope.orders = $http.get(){
-	
-}
+orderApp.controller('OrderCtrl', function ($scope, $http) {
+
+		// Get the venues
+	  $scope.orders = $http.get("http://localhost:5000/orders")
+	  	.success(function(data){
+
+		$scope.orders = data;
+
+	});
+});
+
+
+orderApp.controller('VenuesOrderCtrl', function($scope, $stateParams, $http){
+
+	var venueId = $stateParams["id"];
+
+	// Get Orders at the Venue
+	$scope.orders = $http.get("http://localhost:5000/venues/orders/"+venueId)
+		.success(function(data){
+
+		$scope.orders = data;
+
+	});
+
+});
